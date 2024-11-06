4f1c4e802f5277a9e4bd77fb9d9ea1465bac07af
annakond1 <anyutakond@gmail.com>
2024-11-07 01:27:53 +0300

    add js commit

--- src/app.js
+++ src/app.js
@@ -1,40 +1,33 @@
-/**
- * Проверка имени пользователя
- * @param {string} name
- * @returns {boolean}
- */
-export const nameIsValid = name => typeof name === 'string' && name.length >= 2 && /^[a-z]+$/.test(name)
-
-/**
- * Удаление пробелов из строки
- *
- * @param {string} text
- * @returns {string}
- */
-export const fullTrim = text => (text ?? '').replace(/\s+/g, '')
-
-/**
- * Подсчёт суммы заказа
- *
- * @param {[{quantity: number, name?: string, price: number}]} items
- * @param {number} discount
- * @returns {number}
- * @throws Вернёт ошибку, если скидка не число и больше 99 или меньше 0
- * @example getTotal([{ price: 10, quantity: 10 }]) // 100
- * @example getTotal([{ price: 10, quantity: 1 }]) // 10
- * @example getTotal([{ price: 10, quantity: 1 }, { price: 10, quantity: 9 }]) // 100
- * @example getTotal([{ price: 10, quantity: 0 }], { price: 10, quantity: 9 }) // 90
- * @example getTotal([{ price: 10, quantity: 10 }], 10) // 90
- * @example getTotal([{ price: 10, quantity: 10 }], 100) // 0
- */
-export const getTotal = (items = [], discount = 0) => {
-  if (typeof discount !== 'number') {
-    throw new Error('Скидка должна быть числом')
-  }
-  if (discount < 0 || discount >= 100) {
-    throw new Error('Процент скидки должен быть от 0 до 99')
-  }
-
-  const total = items.reduce((acc, { price, quantity }) => acc + price * quantity, 0)
-  return total * (1 - discount / 100)
+const scores = {
+  Anna: 10,
+  Olga: 1,
+  Ivan: 5
 }
+let arr1 = Object.keys(scores).map(key => scores[key]);
+//function getScore() {
+//function getScore(...valuess) {
+  //let sum = 0;
+  //for(let i = 0; i < arr1.length; i++) {
+    //if(arr1[i] > 0){
+      //getScore = getScore + arr1[i]
+    //}
+  //}
+  //return getScore
+//}
+//console.log(getScore())
+//console.log(arr1)
+
+//leg sum = 0;
+//arr1.forEach(function (el) {
+//sum = sum + el;
+//});
+
+function getScore(array) {
+  let sum = 0;
+  for(let i = 0; i < array.length; i++) {
+    sum += array[i]
+  }
+  console.log(sum);
+}
+
+getScore(arr1)
